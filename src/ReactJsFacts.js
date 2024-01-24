61b66309217a886602270d19c0f8c4f83a90a07e
BhanuSharma09 <bhanusharma66432@gmail.com>
2024-01-24 16:39:10 +0530

    Your Message Here

---
+++ src/ReactJsFacts.js
@@ -0,0 +1,11 @@
+import Main from "./components/Main.js";
+import Navbar from "./components/Navbar.js";
+import './ReactJsFacts.css'
+export default function ReactJsFacts(){
+    return(
+        <div>
+            <Navbar/>
+            <Main/>
+        </div>
+    )
+}
